d1fed7fadbf172b73ec61fd9c61fa6e62556a228
Chirag Ghosh <cghosh828049@gmail.com>
2023-09-12 16:54:44 +0530

    feat: complete part C;

--- ass-one/part-c/Movie-Booking-System.sol
+++ ass-one/part-c/Movie-Booking-System.sol
@@ -40,11 +40,47 @@ contract TicketBooking {
     function buyTicket(
         string memory email,
         uint numTickets
-    ) public payable soldOut {}
+    ) public payable soldOut {
+        require(
+            msg.value >= numTickets * price,
+            "Transaction value shoould not be less than total price of the tickets."
+        );
 
-    function withdrawFunds() public onlyOwner {}
+        Buyer storage buyer = BuyersPaid[msg.sender];
 
-    function refundTicket(address buyer) public onlyOwner {}
+        if (buyer.numTickets > 0) {
+            buyer.numTickets += numTickets;
+            buyer.totalPrice += numTickets * price;
+        } else {
+            buyer.email = email;
+            buyer.numTickets = numTickets;
+            buyer.totalPrice = numTickets * price;
+        }
+
+        uint extra = msg.value - numTickets * price;
+        if (extra > 0) {
+            payable(msg.sender).transfer(extra);
+        }
+    }
+
+    function withdrawFunds() public onlyOwner {
+        uint256 totalFunds = address(this).balance;
+        payable(msg.sender).transfer(totalFunds);
+    }
+
+    function refundTicket(address buyer) public onlyOwner {
+        Buyer storage refundBuyer = BuyersPaid[buyer];
+        require(refundBuyer.numTickets > 0, "Buyer has no ticket booked");
+        require(
+            address(this).balance >= refundBuyer.totalPrice,
+            "Contract has no funds left. Cannot refund"
+        );
+
+        payable(buyer).transfer(refundBuyer.totalPrice);
+        numTicketsSold -= refundBuyer.numTickets;
+        refundBuyer.numTickets = 0;
+        refundBuyer.totalPrice = 0;
+    }
 
     function getBuyerAmountPaid(
         address buyer
@@ -52,5 +88,7 @@ contract TicketBooking {
         totalPaid = BuyersPaid[buyer].totalPrice;
     }
 
-    function kill() public onlyOwner {}
+    function kill() public onlyOwner {
+        selfdestruct(payable(msg.sender));
+    }
 }
